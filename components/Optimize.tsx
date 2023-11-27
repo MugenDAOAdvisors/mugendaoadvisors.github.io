44d58c72cb23a1be35b39c1e4a2346e467b0c2f6
Ricardo Ribeiro <richy.jsr@hotmail.com>
2023-11-27 16:54:37 +0000

    Typing error

--- components/Optimize.tsx
+++ components/Optimize.tsx
@@ -2,7 +2,7 @@ export default function Optimize(){
     return (
             <div className="bg-[#252525] rounded-t-[40px] 2xl:p-[100px] p-[20px] flex justify-between items-center">
                 <div className="w-full md:w-1/2 lg:w-1/3 flex flex-col space-y-[50px]">                
-                    <h2>Optizime your DAO</h2>
+                    <h2>Optimize your DAO</h2>
                     <p className="text-[#7E7E7E]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</p>
                     <div className="flex">
                         <p className="text-black lg:px-[110px] lg:py-[20px] py-[10px] px-[20px] text-center bg-[#EE6FA3] rounded-full ">View All Now</p>
