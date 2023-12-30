f03b8c4b40c103cc3bb2e03f048c311bd7cc58d9
Ricardo Ribeiro <richy.jsr@hotmail.com>
2023-12-30 20:21:19 +0000

    Image Cherry Updte

--- components/Home.tsx
+++ components/Home.tsx
@@ -9,14 +9,15 @@ export default function Home(){
                 <p className="text-black text-[16px] lg:text-[22px] font-medium lg:py-[20px] lg:px-[40px] py-[10px] px-[20px] bg-[#EE6FA3] rounded-full">Explore Now</p>
             </div>
         </div>
-        <div className="relative w-1/3">
-            <img src="/mugen_cherry/mugen_cherry.png" className="2xl:top-0 top-20 right-0 hidden lg:block"></img>
+        <div className="hidden lg:block relative w-1/3">
+            <img src="mugen_cherry.png"></img>
+            {/* <img src="/mugen_cherry/mugen_cherry.png" className="2xl:top-0 top-20 right-0 hidden lg:block"></img>
             <img src="/mugen_cherry/TopRightPetal.png" className="absolute top-[70px] right-[180px] w-[5%] hidden lg:block topRightPetal"></img>
             <img src="/mugen_cherry/TopPetal.png" className="absolute top-[30px] right-[330px] w-[5%] hidden lg:block topPetal"></img>
             <img src="/mugen_cherry/TopLeftPetal.png" className="absolute top-[190px] right-[390px] w-[5%] hidden lg:block topLeftPetal"></img>
             <img src="/mugen_cherry/BottomRightPetal.png" className="absolute top-[400px] right-[250px] w-[3%] hidden lg:block bottomRightPetal"></img>
             <img src="/mugen_cherry/BottomPetal.png" className="absolute top-[430px] right-[330px] w-[4%] hidden lg:block bottomPetal"></img>
-            <img src="/mugen_cherry/BottomLeftPetal.png" className="absolute top-[480px] right-[410px] w-[5%] hidden lg:block bottomLeftPetal"></img>
+            <img src="/mugen_cherry/BottomLeftPetal.png" className="absolute top-[480px] right-[410px] w-[5%] hidden lg:block bottomLeftPetal"></img> */}
         </div>
     </div>
     )
