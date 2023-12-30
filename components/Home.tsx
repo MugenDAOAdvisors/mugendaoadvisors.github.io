7b226e334586ed7ebb56c04629ee183fc22f14c0
Ricardo Ribeiro <richy.jsr@hotmail.com>
2023-12-30 13:18:27 +0000

    Service animation

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,14 +1,23 @@
+
 export default function Home(){
     return(
-    <div className="relative w-full">
-        <div className="flex flex-col space-y-[50px]">
-            <h1 className="2xl:w-2/3">Elevator & innovative solutions</h1>
-            <p className="lg:w-[65%] 2xl:w-[35%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
+    <div className="w-full flex">
+        <div className="flex flex-col space-y-[50px] w-2/3">
+            <h1 className="">Elevator & innovative solutions</h1>
+            <p className="lg:w-[65%] 2xl:w-[65%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
             <div className="flex">
                 <p className="text-black text-[16px] lg:text-[22px] font-medium lg:py-[20px] lg:px-[40px] py-[10px] px-[20px] bg-[#EE6FA3] rounded-full">Explore Now</p>
             </div>
         </div>
-        <img src="/mugen_cherry.png" className="absolute 2xl:top-0 top-20 right-0 w-[30%] hidden lg:block"></img>
+        <div className="relative w-1/3">
+            <img src="/mugen_cherry/mugen_cherry.png" className="2xl:top-0 top-20 right-0 hidden lg:block"></img>
+            <img src="/mugen_cherry/TopRightPetal.png" className="absolute top-[70px] right-[180px] w-[5%] hidden lg:block topRightPetal"></img>
+            <img src="/mugen_cherry/TopPetal.png" className="absolute top-[30px] right-[330px] w-[5%] hidden lg:block topPetal"></img>
+            <img src="/mugen_cherry/TopLeftPetal.png" className="absolute top-[190px] right-[390px] w-[5%] hidden lg:block topLeftPetal"></img>
+            <img src="/mugen_cherry/BottomRightPetal.png" className="absolute top-[400px] right-[250px] w-[3%] hidden lg:block bottomRightPetal"></img>
+            <img src="/mugen_cherry/BottomPetal.png" className="absolute top-[430px] right-[330px] w-[4%] hidden lg:block bottomPetal"></img>
+            <img src="/mugen_cherry/BottomLeftPetal.png" className="absolute top-[480px] right-[410px] w-[5%] hidden lg:block bottomLeftPetal"></img>
+        </div>
     </div>
     )
 }
--- pages/services.tsx
+++ pages/services.tsx
@@ -1,97 +1,167 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { useEffect, useRef, useState } from "react";
 
-export default function Services (){
-    return (
-        <div>
-            <section className="2xl:px-[130px] lg:px-[50px] px-[20px]">
-                <Navbar></Navbar>
-            </section>
-            <section className="2xl:mx-[130px] lg:mx-[50px] mx-[20px] pb-[300px] border-b-[1px]  border-[#D9D9D9] mt-[150px]">
-                <div className="flex flex-col space-y-[80px]">
-                    <h1 className="text-center">TAKE AIM AT SUCCESS WITH OUR PRECISION <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#673147] from-[-30%] to-[#FFFFFF] to-[400%]">DAO SOLUTIONS</span></h1>
-                    <p className="text-center">Our expertise in crafting unique and innovative designs allows us to deliver comprehensive end-to-end projects that build lasting connections.</p>
-                </div>
-            </section>
+export default function Services() {
+const capabilities = [
+{
+    title: "Community Support",
+    topics: [
+    "Lorem ipsum dolor sit amet 1",
+    "Lorem ipsum dolor sit amet 2",
+    "Lorem ipsum dolor sit amet 3",
+    "Lorem ipsum dolor sit amet 4",
+    ],
+},
+{
+    title: "DAO governance",
+    topics: [
+    "Lorem ipsum dolor sit amet 1",
+    "Lorem ipsum dolor sit amet 2",
+    "Lorem ipsum dolor sit amet 3",
+    "Lorem ipsum dolor sit amet 4",
+    ],
+},
+{
+    title: "Strategic Advisory",
+    topics: [
+    "Lorem ipsum dolor sit amet 1",
+    "Lorem ipsum dolor sit amet 2",
+    "Lorem ipsum dolor sit amet 3",
+    "Lorem ipsum dolor sit amet 4",
+    ],
+},
+];
 
-            <section className="2xl:px-[130px] lg:px-[50px] px-[20px] mt-[70px] mb-[150px]">
-                <div className="flex flex-col lg:flex-row lg:space-x-[120px]">
-                    <div className="lg:w-1/3">
-                        <h2 className="text-[40px]">
-                            CAPABILITIES IN OUR SERVICES
-                        </h2>
-                    </div>
-                    <div className="flex flex-col w-full">
-                        <div className=" mt-[70px] flex flex-col space-y-[70px] border-b-[1px] border-[#D9D9D9] w-full pb-[115px]">
-                            <h2 className="text-[40px]">
-                                Community Support
-                            </h2>
-                            <div className="flex flex-col lg:space-y-[50px] space-y-[20px]">
-                                <div className="flex flex-col lg:flex-row lg:space-x-[100px] space-y-[20px]">
-                                    <p className="list-item list-disc ml-[30px]">Lorem ipsum dolor sit amet</p>
-                                    <p className="list-item list-disc ml-[30px]">Lorem ipsum dolor sit amet</p>
-                                </div>
-                                <div className="flex flex-col lg:flex-row lg:space-x-[100px] space-y-[20px]">
-                                    <p className="list-item list-disc ml-[30px]">Lorem ipsum dolor sit amet</p>
-                                    <p className="list-item list-disc ml-[30px]">Lorem ipsum dolor sit amet</p>
-                                </div>
-                            </div>
-                        </div> 
-                    </div>
-                    
-                </div>
-            </section>
-            <section className="2xl:px-[130px] lg:px-[50px] px-[20px]">
-                <div className="bg-[#673147] 2xl:px-[150px] lg:px-[50px] py-[90px] rounded-[30px] relative">
-                    <img src="/mugen_koi.png" className="absolute hidden lg:block -right-[5%] -top-[30%]"></img>
-                    <h2 className="text-[40px] text-center mb-[100px]">
-                        OUR PROCESS
-                    </h2>
-                    <div className="flex flex-col lg:space-x-[50px] lg:flex-row justify-between space-y-[50px] lg:space-y-0">
-                        <div className="flex flex-col space-y-[10] lg:space-y-[25px] items-center text-center lg:w-1/3">
-                            <h3 className="text-[54px]">
-                                01
-                            </h3>
-                            <h3 className="text-[32px]">
-                                Strategy
-                            </h3>
-                            <p>Lorem ipsum dolor sit amet</p>
-                        </div>
+const titleRef = useRef<HTMLObjectElement>(null)
+const ref = useRef<HTMLElement>(null)
+const [scrollOffset, setScrollOffset] = useState(0);
+const [lastScrollTop, setLastScrollTop] = useState(0);
 
-                        <div className="flex flex-col space-y-[10] lg:space-y-[25px] items-center text-center lg:w-1/3">
-                            <h3 className="text-[54px]">
-                                02
-                            </h3>
-                            <h3 className="text-[32px]">
-                                System Creation
-                            </h3>
-                            <p>Lorem ipsum dolor sit amet</p>
-                        </div>
 
-                        <div className="flex flex-col space-y-[10] lg:space-y-[25px] items-center text-center lg:w-1/3">
-                            <h3 className="text-[54px]">
-                                03
-                            </h3>
-                            <h3 className="text-[32px]">
-                                Implementation
-                            </h3>
-                            <p>Lorem ipsum dolor sit amet</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="2xl:px-[130px] lg:px-[50px] px-[20px] mb-[130px] mt-[250px]">
-                <div className="flex flex-col lg:flex-row  px-[20px] py-[20px] lg:px-[50px] 2xl:px-[150px] lg:py-[90px] space-y-[100px] lg:space-y-[0] items-center justify-between rounded-[30px]">
-                    <div className="flex flex-col space-y-[30px] lg:w-1/2 2xl:w-1/3 ">
-                        <h1>Optimize Your DAO</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                    </div>
-                    <div>
-                        <p className="text-black px-[80px] py-[20px] font-bold bg-[#EE6FA3] rounded-full ">Let's Chat</p>
-                    </div>
-                </div>
-            </section>
-            <Footer></Footer>
+
+useEffect(() => {
+    const handleScroll = () => {
+        if (ref.current && titleRef.current) {
+            const textRect =  titleRef.current.getBoundingClientRect()
+            const rect = ref.current.getBoundingClientRect();
+            const currentScrollTop = rect.top
+            
+            if(rect.top < 0 && rect.bottom > textRect.bottom ){
+                setScrollOffset(-rect.top);
+                setLastScrollTop(currentScrollTop);
+            }
+            else if(currentScrollTop > lastScrollTop && rect.top < textRect.top){
+                setScrollOffset(-rect.top);
+            }
+        }
+    };
+
+    // Adiciona um ouvinte de evento de rolagem
+    window.addEventListener("scroll", handleScroll);
+
+    // Remove o ouvinte de evento ao desmontar o componente
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [ref, lastScrollTop, titleRef]);
+
+
+
+return (
+<div>
+    <section className="2xl:px-[130px] lg:px-[50px] px-[20px]">
+    <Navbar></Navbar>
+    </section>
+    <section className="2xl:mx-[130px] lg:mx-[50px] mx-[20px] pb-[300px] border-b-[1px]  border-[#D9D9D9] mt-[150px]">
+    <div className="flex flex-col space-y-[80px] items-center">
+        <h1 className="text-center">
+        TAKE AIM AT SUCCESS WITH OUR PRECISION{" "}
+        <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#673147] from-[-30%] to-[#FFFFFF] to-[400%]">
+            DAO SOLUTIONS
+        </span>
+        </h1>
+        <p className="text-center w-1/2">
+        Our expertise in crafting unique and innovative designs allows us to
+        deliver comprehensive end-to-end projects that build lasting
+        connections.
+        </p>
+    </div>
+    </section>
+    <section ref={ref} className="2xl:px-[130px] lg:px-[50px] px-[20px] pt-[70px] mb-[150px] relative">
+    <div className="flex flex-col lg:flex-row lg:space-x-[120px] ">
+        <div className="lg:w-1/3">
+        <h2 ref={titleRef} className={`absolute text-[40px] top-[70px]`} style={{ top: `${scrollOffset}px`  }}>CAPABILITIES <br></br>IN OUR SERVICES</h2>
+       
         </div>
-    )
+        <div className="flex flex-col w-full">
+        {capabilities.map((capability, index) => (
+            <div
+            key={index}
+            className="mt-[70px] flex flex-col space-y-[70px] border-b-[1px] border-[#D9D9D9] w-full pb-[115px]"
+            >
+            <h2 className="text-[40px]">{capability.title}</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-[50px] ">
+                {capability.topics.map((topic, topicIndex) => (
+                <p
+                    key={topicIndex}
+                    className="list-item list-disc ml-[30px]"
+                >
+                    {topic}
+                </p>
+                ))}
+            </div>
+            </div>
+        ))}
+        </div>
+    </div>
+    </section>
+
+    <section className="2xl:px-[130px] lg:px-[50px] px-[20px]">
+        <div className="bg-[#673147] 2xl:px-[150px] lg:px-[50px] py-[90px] rounded-[30px] relative">
+            <img
+            src="/mugen_koi.png"
+            className="absolute hidden lg:block -right-[5%] -top-[30%]"
+            ></img>
+            <h2 className="text-[40px] text-center mb-[100px]">OUR PROCESS</h2>
+            <div className="flex flex-col lg:space-x-[50px] lg:flex-row justify-between space-y-[50px] lg:space-y-0">
+            <div className="flex flex-col space-y-[10] lg:space-y-[25px] items-center text-center lg:w-1/3">
+                <h3 className="text-[54px]">01</h3>
+                <h3 className="text-[32px]">Strategy</h3>
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+
+            <div className="flex flex-col space-y-[10] lg:space-y-[25px] items-center text-center lg:w-1/3">
+                <h3 className="text-[54px]">02</h3>
+                <h3 className="text-[32px]">System Creation</h3>
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+
+            <div className="flex flex-col space-y-[10] lg:space-y-[25px] items-center text-center lg:w-1/3">
+                <h3 className="text-[54px]">03</h3>
+                <h3 className="text-[32px]">Implementation</h3>
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+            </div>
+        </div>
+    </section>
+    <section className="2xl:px-[130px] lg:px-[50px] px-[20px] mb-[130px] mt-[250px]">
+    <div className="flex flex-col lg:flex-row  px-[20px] py-[20px] lg:px-[50px] 2xl:px-[150px] lg:py-[90px] space-y-[100px] lg:space-y-[0] items-center justify-between rounded-[30px]">
+        <div className="flex flex-col space-y-[30px] lg:w-1/2 2xl:w-1/3 ">
+        <h1>Optimize Your DAO</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </p>
+        </div>
+        <div>
+        <p className="text-black px-[80px] py-[20px] font-bold bg-[#EE6FA3] rounded-full ">
+            Let's Chat
+        </p>
+        </div>
+    </div>
+    </section>
+    <Footer></Footer>
+</div>
+);
 }
